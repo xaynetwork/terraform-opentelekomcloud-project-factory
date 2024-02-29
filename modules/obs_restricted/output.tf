8e905ff54466b636c1769ea3ffdd0be4194432d2
Jorge E. Gamboa G. <jogamboa4@gmail.com>
2024-02-29 10:29:37 -0600

    Adding sensitive to output

--- modules/obs_restricted/output.tf
+++ modules/obs_restricted/output.tf
@@ -3,9 +3,11 @@ output "bucket_name" {
 }
 
 output "bucket_access_key" {
-  value = opentelekomcloud_identity_credential_v3.user_aksk.access
+  value     = opentelekomcloud_identity_credential_v3.user_aksk.access
+  sensitive = true
 }
 
 output "bucket_secret_key" {
-  value = opentelekomcloud_identity_credential_v3.user_aksk.secret
+  value     = opentelekomcloud_identity_credential_v3.user_aksk.secret
+  sensitive = true
 }
